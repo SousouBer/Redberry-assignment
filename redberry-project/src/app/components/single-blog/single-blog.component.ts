a1b4c35c0dd0719e3e3a9eeb24f0ba9b0d804b26
Soso Beriashvili <sosiberiashvili@gmail.com>
2023-12-22 13:31:18 +0400

    feat: complete markup for blog's detailed info page

--- redberry-project/src/app/components/single-blog/single-blog.component.ts
+++ redberry-project/src/app/components/single-blog/single-blog.component.ts
@@ -2,25 +2,46 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Blog, Category } from 'src/app/models/models.interface';
 import { BlogsService } from 'src/app/services/blogs.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, take } from 'rxjs';
+import { BlogItemComponent } from '../blog-item/blog-item.component';
 
 @Component({
   selector: 'app-single-blog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BlogItemComponent],
   templateUrl: './single-blog.component.html',
-  styleUrls: ['./single-blog.component.scss']
+  styleUrls: ['./single-blog.component.scss'],
 })
 export class SingleBlogComponent implements OnInit {
   @Input() blogItem!: Blog;
-   // Subject to hold all categories from the service.
-   categories$!: Observable<Category[]>;
 
-   constructor(private blogsService: BlogsService){}
+  //  Subject for holding all blogs in one place.
+  allBlogs$!: Observable<Blog[]>;
+
+  // Subject to hold all categories from the service.
+  categories$!: Observable<Category[]>;
+
+  // Subject to hold blogs that have similar categories.
+  // similarBlogs$!: Observable<Blog[]>;
+  similarBlogs$ = new BehaviorSubject<Blog[]>([]);
+  constructor(private blogsService: BlogsService) {}
 
   ngOnInit(): void {
     this.categories$ = this.blogsService.getCategories();
+    this.allBlogs$ = this.blogsService.getBlogs();
+
+    // Take only those blogs that contain at least one same category.
+    this.allBlogs$.pipe(take(1)).subscribe((blogs) => {
+      const similarBlogs = blogs.filter((blog) =>
+        this.blogItem.categories.some((category) =>
+          blog.categories.includes(category)
+        )
+      );
+
+      this.similarBlogs$.next(similarBlogs);
+    });
 
     this.blogsService.init();
+    this.blogsService.loadBlogs();
   }
 }
